4905545b9989b179097cf2670f3235152c7add06
pedro2398 <pedrooo2398@gmail.com>
2025-05-31 09:43:06 -0300

    chore: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
