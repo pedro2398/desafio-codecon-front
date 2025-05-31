8fba7f8305162bf124c7013434c5f000c3ddcc00
pedro2398 <pedrooo2398@gmail.com>
2025-05-31 09:46:42 -0300

    chore: implements components in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { Button } from "@/components/button";
+import ThemeToggle from "@/components/themeToggle";
+
 export default function Home() {
-  return <h1 className="flex justify-center text-green-500 text-3xl font-bold">Desafio Codecon</h1>;
+  return (
+    <div className="flex flex-col  items-center w-full h-screen py-4 gap-4">
+      <h1 className="text-green-500 text-3xl font-bold">Desafio Codecon</h1>
+      <Button name="testButton" func={() => console.log("clicado!")} />
+      <ThemeToggle />
+    </div>
+  );
 }
